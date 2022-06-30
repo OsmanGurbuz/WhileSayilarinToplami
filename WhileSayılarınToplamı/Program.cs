0cea58a2507075f0fbef5ccc751f38c8f342d438
OsmanGurbuz <107862900+OsmanGurbuz@users.noreply.github.com>
2022-06-30 14:19:08 +0300

    While ile sayıların toplama işlemi
    While ile sayıların toplama işlemi

---
+++ WhileSayılarınToplamı/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileSayılarınToplamı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 1;
+            int sayi = 0;
+            int toplam = 0;
+
+            while (sayac<=5)
+            {
+                Console.Write("{0}. sayıyı giriniz", sayac);
+                sayi = Convert.ToInt32(Console.ReadLine());
+                toplam = toplam + sayi;
+                sayac++;
+
+
+            }
+            Console.Write("sayıların toplamı:{0}",toplam);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WhileSayılarınToplamı/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("WhileSayılarınToplamı")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WhileSayılarınToplamı")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("5aec6f1f-2207-4f37-bb31-4849b20a3af6")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
